8af67dddb1baa5c81f638adbf66208afc782bfbd
JagodaJakub <kubajagjag@gmail.com>
2025-04-03 23:16:02 +0200

    Added LineDrawerAdapter

---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/LineDrawerAdapter.java
@@ -0,0 +1,40 @@
+package edu.kis.powp.jobs2d.drivers.adapter;
+
+import edu.kis.legacy.drawer.panel.DrawPanelController;
+import edu.kis.legacy.drawer.shape.ILine;
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+public class LineDrawerAdapter extends DrawPanelController implements Job2dDriver {
+
+    private final DrawPanelController drawPanelController;
+    private final ILine line;
+
+    private int startX = 0;
+    private int startY = 0;
+
+    public LineDrawerAdapter(DrawPanelController drawPanelController, ILine line) {
+        this.drawPanelController = drawPanelController;
+        this.line = line;
+    }
+
+    @Override
+    public void setPosition(int x, int y) {
+        this.startX = x;
+        this.startY = y;
+    }
+
+    @Override
+    public void operateTo(int x, int y) {
+        this.line.setStartCoordinates(this.startX, this.startY);
+        this.line.setEndCoordinates(x, y);
+        this.setPosition(x, y);
+
+        this.drawPanelController.drawLine(this.line);
+    }
+
+    @Override
+    public String toString() {
+        return "LineDrawerAdapter";
+    }
+
+}
