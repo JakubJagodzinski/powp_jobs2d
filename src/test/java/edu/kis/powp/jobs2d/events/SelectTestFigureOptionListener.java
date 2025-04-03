612f926dc66c99b638af21ec1ec6de48c54c8ffd
JagodaJakub <kubajagjag@gmail.com>
2025-04-03 23:40:22 +0200

    Added more figure drawing options

--- src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
@@ -2,13 +2,29 @@ package edu.kis.powp.jobs2d.events;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Objects;
 
 import edu.kis.powp.jobs2d.drivers.DriverManager;
+import edu.kis.powp.jobs2d.drivers.adapter.AdapterFromJob2DDriverToAbstractDriver;
+import edu.kis.powp.jobs2d.magicpresets.FiguresJane;
 import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
 
 public class SelectTestFigureOptionListener implements ActionListener {
 
 	private DriverManager driverManager;
+
+	private enum Figure {
+		FIGURE_JOE_1("Figure Joe 1"),
+		FIGURE_JOE_2("Figure Joe 2"),
+		FIGURE_JANE_1("Figure Jane 1");
+
+		private final String figureName;
+
+		Figure(String figureName) {
+			this.figureName = figureName;
+		}
+
+	}
 
 	public SelectTestFigureOptionListener(DriverManager driverManager) {
 		this.driverManager = driverManager;
@@ -16,6 +32,12 @@ public class SelectTestFigureOptionListener implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		FiguresJoe.figureScript1(driverManager.getCurrentDriver());
+		if(Objects.equals(e.getActionCommand(), Figure.FIGURE_JOE_1.figureName)) {
+			FiguresJoe.figureScript1(driverManager.getCurrentDriver());
+		} else if(Objects.equals(e.getActionCommand(), Figure.FIGURE_JOE_2.figureName)) {
+			FiguresJoe.figureScript2(driverManager.getCurrentDriver());
+		} else if(Objects.equals(e.getActionCommand(), Figure.FIGURE_JANE_1.figureName)) {
+			FiguresJane.figureScript(new AdapterFromJob2DDriverToAbstractDriver(driverManager.getCurrentDriver()));
+		}
 	}
 }
