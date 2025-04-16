a2257b334a18ebe9a40b1f7538b9921cb4e91694
JagodaJakub <kubajagjag@gmail.com>
2025-04-16 22:31:27 +0200

    Small fixes: - Removed unnecessary semicolon in Job2dDriverTest.java - Replaced function with lambda in TestJobs2dPatterns.java - Removed unnecessary inheritance in LineDrawerAdapter.java

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/LineDrawerAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/LineDrawerAdapter.java
@@ -4,7 +4,7 @@ import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.powp.jobs2d.Job2dDriver;
 
-public class LineDrawerAdapter extends DrawPanelController implements Job2dDriver {
+public class LineDrawerAdapter implements Job2dDriver {
 
     private final DrawPanelController drawPanelController;
     private final ILine line;
--- src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
+++ src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
@@ -87,19 +87,17 @@ public class TestJobs2dPatterns {
      * Launch the application.
      */
     public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                Application app = new Application("2d jobs Visio");
-                DrawerFeature.setupDrawerPlugin(app);
-                setupDefaultDrawerVisibilityManagement(app);
+        EventQueue.invokeLater(() -> {
+            Application app = new Application("2d jobs Visio");
+            DrawerFeature.setupDrawerPlugin(app);
+            setupDefaultDrawerVisibilityManagement(app);
 
-                DriverFeature.setupDriverPlugin(app);
-                setupDrivers(app);
-                setupPresetTests(app);
-                setupLogger(app);
+            DriverFeature.setupDriverPlugin(app);
+            setupDrivers(app);
+            setupPresetTests(app);
+            setupLogger(app);
 
-                app.setVisibility(true);
-            }
+            app.setVisibility(true);
         });
     }
 
