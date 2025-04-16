75c4af7ebfdc7fd4576893636262a9ea098ea838
JagodaJakub <kubajagjag@gmail.com>
2025-04-16 22:22:08 +0200

    Moved Figure enum to another file

---
+++ src/test/java/edu/kis/powp/jobs2d/Figure.java
@@ -0,0 +1,18 @@
+package edu.kis.powp.jobs2d;
+
+public enum Figure {
+
+    FIGURE_JOE_1("Figure Joe 1"),
+    FIGURE_JOE_2("Figure Joe 2"),
+    FIGURE_JANE_1("Figure Jane 1"),
+    FIGURE_RECTANGLE_1("Figure Rectangle 1"),
+    FIGURE_TRIANGLE_1("Figure Triangle 1");
+
+    public final String figureName;
+
+    Figure(String figureName) {
+        this.figureName = figureName;
+    }
+
+
+}
--- src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
@@ -1,54 +1,39 @@
 package edu.kis.powp.jobs2d.events;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.Objects;
-
+import edu.kis.powp.jobs2d.Figure;
 import edu.kis.powp.jobs2d.drivers.DriverManager;
 import edu.kis.powp.jobs2d.drivers.adapter.AdapterFromJob2DDriverToAbstractDriver;
-import edu.kis.powp.jobs2d.factories.FigureFactory;
 import edu.kis.powp.jobs2d.factories.RectangleFactory;
 import edu.kis.powp.jobs2d.factories.TriangleFactory;
 import edu.kis.powp.jobs2d.magicpresets.FiguresJane;
 import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Objects;
+
 public class SelectTestFigureOptionListener implements ActionListener {
 
-	private DriverManager driverManager;
+    private DriverManager driverManager;
 
-	private enum Figure {
-		FIGURE_JOE_1("Figure Joe 1"),
-		FIGURE_JOE_2("Figure Joe 2"),
-		FIGURE_JANE_1("Figure Jane 1"),
-		FIGURE_RECTANGLE_1("Figure Rectangle 1"),
-		FIGURE_TRIANGLE_1("Figure Triangle 1");
+    public SelectTestFigureOptionListener(DriverManager driverManager) {
+        this.driverManager = driverManager;
+    }
 
-		private final String figureName;
-
-		Figure(String figureName) {
-			this.figureName = figureName;
-		}
-
-	}
-
-	public SelectTestFigureOptionListener(DriverManager driverManager) {
-		this.driverManager = driverManager;
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		if(Objects.equals(e.getActionCommand(), Figure.FIGURE_JOE_1.figureName)) {
-			FiguresJoe.figureScript1(driverManager.getCurrentDriver());
-		} else if(Objects.equals(e.getActionCommand(), Figure.FIGURE_JOE_2.figureName)) {
-			FiguresJoe.figureScript2(driverManager.getCurrentDriver());
-		} else if(Objects.equals(e.getActionCommand(), Figure.FIGURE_JANE_1.figureName)) {
-			FiguresJane.figureScript(new AdapterFromJob2DDriverToAbstractDriver(driverManager.getCurrentDriver()));
-		} else if(Objects.equals(e.getActionCommand(), Figure.FIGURE_RECTANGLE_1.figureName)) {
-			RectangleFactory rectangleFactory = new RectangleFactory(0, 0);
-			rectangleFactory.createScript(200, 200).execute(driverManager.getCurrentDriver());
-		} else if(Objects.equals(e.getActionCommand(), Figure.FIGURE_TRIANGLE_1.figureName)) {
-			TriangleFactory triangleFactory = new TriangleFactory(0, 0);
-			triangleFactory.createScript(200, 200).execute(driverManager.getCurrentDriver());
-		}
-	}
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (Objects.equals(e.getActionCommand(), Figure.FIGURE_JOE_1.figureName)) {
+            FiguresJoe.figureScript1(driverManager.getCurrentDriver());
+        } else if (Objects.equals(e.getActionCommand(), Figure.FIGURE_JOE_2.figureName)) {
+            FiguresJoe.figureScript2(driverManager.getCurrentDriver());
+        } else if (Objects.equals(e.getActionCommand(), Figure.FIGURE_JANE_1.figureName)) {
+            FiguresJane.figureScript(new AdapterFromJob2DDriverToAbstractDriver(driverManager.getCurrentDriver()));
+        } else if (Objects.equals(e.getActionCommand(), Figure.FIGURE_RECTANGLE_1.figureName)) {
+            RectangleFactory rectangleFactory = new RectangleFactory(0, 0);
+            rectangleFactory.createScript(200, 200).execute(driverManager.getCurrentDriver());
+        } else if (Objects.equals(e.getActionCommand(), Figure.FIGURE_TRIANGLE_1.figureName)) {
+            TriangleFactory triangleFactory = new TriangleFactory(0, 0);
+            triangleFactory.createScript(200, 200).execute(driverManager.getCurrentDriver());
+        }
+    }
 }
